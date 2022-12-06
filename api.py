9f96701fa3adbfaab43ad5b962a7974177de5665
Abou Kaossar BAWA <bawakasiskley1@gmail.com>
2022-12-06 23:04:22 +0100

    ajout des fichiers finaux pour gérer lapi et le petit site générer avec streamlit

--- api.py
+++ api.py
@@ -1,20 +1,12 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import sys
 import io
 import numpy as np
-
-import requests
-# from model import load_data, initialize_model, fit_model, predict
-
-from tensorflow.keras import layers, models, Sequential
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.callbacks import EarlyStopping
-from matplotlib.pyplot import imread
-from tensorflow.image import resize
+import tensorflow as tf
 from tensorflow import expand_dims
 
-model = models.load_model('./model-abou')
+model = tf.keras.models.load_model('./raw_data/saved_model/model')
 input_shape = model.layers[0].input_shape
 
 app = FastAPI()
@@ -24,25 +16,16 @@ app = FastAPI()
 def index():
     return {'ok': "bienvenue dans l'index"}
 
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
-
 @app.post('/predict')
 async def predict_image(image: UploadFile = File(...)):
 
     try:
         contents = await image.read()
         pil_image = Image.open(io.BytesIO(contents))
-        # .resize((input_shape[1], input_shape[2]))
         pic = np.array(pil_image)
-        image_scaled = pic/255.
-        image_final = expand_dims(image_scaled, 0)
+        image_final = expand_dims(pic, 0)
         res = model.predict(image_final)
         print(res)
-        return {"result": float(res[0][0])}
-
+        return {"result": round(float(res[0][0]),2)}
     except:
-        e = sys.exc_info()[1]
-		# raise HTTPException(status_code=500, detail=str(e))
+        return {"error": str(sys.exc_info()[1])}
--- app.py
+++ app.py
@@ -1,55 +1,40 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-import datetime
 import requests
-import os
-import matplotlib.pyplot as plt
-from scipy import misc
 from PIL import Image
-# import cv2
-from io import BytesIO
-
+import io
 
 '''
 # DEEPFAKE HUNTER APPLICATION
+
 '''
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+url = 'http://127.0.0.1:8000/predict'
 
 uploaded_files = st.file_uploader("Choose photos to upload", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'])
-st.set_option('deprecation.showfileUploaderEncoding', False) # Enabling the automatic file decoder
 
-if st.button('AFFICHER l \'IMAGE'):
+col1, col2,col3 = st.columns([1,5,1])
+with col1:
+    if st.button('Afficher'):
 
-    image = Image.open(uploaded_files)
+        contents = uploaded_files.read()
+        pil_image = Image.open(io.BytesIO(contents))
+        image = np.array(pil_image)
+        st.image(image, channels="BGR")
 
-    fig = plt.figure(2, figsize=(200, 200))
-    fig, ax = plt.subplots()
+with col2:
+    if st.button('Prédire'):
 
+        files={'image' : uploaded_files}
 
-    st.image(image, width=500)
-
-
-
-url = 'http://127.0.0.1:8000/predict'
-if url == 'http://127.0.0.1:8000/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-if st.button('predict'):
-
-    files={'image' : uploaded_files}
-
-    response = requests.post(url, files=files)
-    if response.status_code == 200:
-        wb = response.json()
-        # wb = response.text
-        # st.write(wb + '  ' + str(response.status_code)
-        # st.write('the prediction  is ', wb['result'])
-        st.write('the prediction  is ', wb["result"])
-    else:
-        st.write('the response.status_code  is ', str(response.status_code) + '   ' + response.text)
-        st.write('the response.status_code  is ', str(response.status_code) + '   ' + uploaded_files.name)
-else:
-        st.write('I was not clicked 😞')
+        response = requests.post(url, files=files)
+        if response.status_code == 200:
+            wb = response.json()
+            if wb['result'] < 0.5:
+                real = '<p style="font-family:sans-serif; color:Green; font-size: 20px;">Real</p>'
+                st.markdown(real, unsafe_allow_html=True)
+            else:
+                fake = '<p style="font-family:sans-serif; color:Red; font-size: 20px;">Fake</p>'
+                st.markdown(fake, unsafe_allow_html=True)
+        else:
+            st.write('Error ', str(response.status_code))
